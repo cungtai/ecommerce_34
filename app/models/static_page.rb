448cc41127a14e7e56f669f83bf3009a7265de13
CUNGTAI <cnptai@hueic.edu.vn>
2017-08-30 21:46:54 +0700

    Finish Static Pages

--- app/models/static_page.rb
+++ app/models/static_page.rb
@@ -1,3 +1,7 @@
 class StaticPage < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :key, presence: true, length: { maximum: 100 }
+
+  scope :of_ids, -> ids {where id: ids}
 end
