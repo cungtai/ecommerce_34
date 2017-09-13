8d40b5cc6c069648d5b79d645f4e250cf786ce3c
binhdev <ttbinh@hueic.edu.vn>
2017-09-13 16:56:17 +0700

    Finish user-role

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < BaseAdminController
+  load_and_authorize_resource
   before_action :authenticate_user!
   before_action :load_product, only: [:destroy, :update, :edit]
 
--- app/controllers/base_admin_controller.rb
+++ app/controllers/base_admin_controller.rb
@@ -1,6 +1,12 @@
 class BaseAdminController < ApplicationController
   layout "layouts/admin/admin_layout"
+
   def all_payments
     Payment.all.map{|p| [p.name,p.id]}
+  end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = exception.message
+    redirect_to root_url
   end
 end
